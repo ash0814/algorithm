bc3b8fef283cb304334f3da97d158b24bfb86742
Ash <69899192+ash0814@users.noreply.github.com>
2023-11-28 20:57:53 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2028 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -14,11 +14,13 @@ int main()
   for (int i = 0; i < 26; i++)
     alpha.insert({'a' + i, i + 1});
 
-  long long ans = 0;
+  unsigned long long ans = 0;
+  unsigned long long r = 1;
   for (int i = 0; i < L; i++)
   {
     int num = alpha[str[i]];
-    ans += num * powl(31, i);
+    ans = (ans + (num * r)) % 1234567891;
+    r = (r * 31) % 1234567891;
   }
-  cout << ans % 1234567891 << endl;
+  cout << ans << endl;
 }
