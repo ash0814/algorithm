3eb6ee8141de0bfea093c6de5a26c99e401bd4ed
Ash <sehyeon014@gmail.com>
2024-04-28 21:05:47 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+int list[10] = {0,};
+int arr[10] = {0, };
+int visited[10] = {0, };
+
+void perm(int cnt, int idx)
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+		return;
+	}
+
+	for (int i = idx; i < N; i++)
+	{
+		if (visited[i] == false) {
+			visited[i] = true;
+			arr[cnt] = list[i];
+			perm(cnt + 1, i);
+			visited[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	cin >> N >> M;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> list[i];
+	}
+	sort(list, list + N);
+
+	perm(0, 0);
+}
