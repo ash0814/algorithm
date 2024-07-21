93554cebc09c138093a6456155acaa3d8c37c63d
Ash <sehyeon014@gmail.com>
2024-07-21 17:32:07 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<int> list;
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
+		list.push_back(num);
+	}
+	sort(list.begin(), list.end());
+
+	cout << list[0] * list[N - 1];
+}
